a6223f357676e75a721c380fc85bebe38cf6a3b4
meandevstar <andrew.norasing@gmail.com>
2023-01-19 04:32:55 -0500

    :white_check_mark: add tests mocks

---
+++ src/tests/mocks/survivors.ts
@@ -0,0 +1,33 @@
+import { Gender } from 'definitions/interfaces';
+import { Router } from 'express';
+
+export const fakeSurvivor = {
+  name: 'John Doe',
+  age: 24,
+  gender: Gender.Male,
+  lastLocation: {
+    lat: 44.44,
+    long: 55.55,
+  },
+  inventory: {
+    WATER: 100,
+    FOOD: 100,
+    MED: 100,
+    AMMO: 100,
+  },
+};
+
+export const reqPlaceholder = {
+  body: {},
+};
+
+export const resPlaceholder = {
+  json() {
+    //
+  },
+};
+
+export const inject = (
+  router: Router,
+  { method, path, position = 0 }: { method: string; path: string; position?: number },
+) => router.stack.find(l => l.route.methods[method] && l.route.path === path).route.stack[position];
