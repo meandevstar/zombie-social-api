db49dfe46391948de70b8be422a0bd07424dbf1e
meandevstar <andrew.norasing@gmail.com>
2023-01-19 04:31:35 -0500

    :sparkles: add survivor endpoints to router

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { INDEX_NAME } from '../env';
+import survivors from '../survivors';
 const router = Router();
 
 router.get('/', (_, res) => {
   res.send(`app-root, ${INDEX_NAME} mode`);
 });
+router.use('/survivors', survivors);
 export default router;
